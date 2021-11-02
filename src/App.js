8c117a2980cd0e051156223e2944f6bd73e053f9
tommy <tommysolano18@hotmail.com>
2021-11-01 21:48:02 -0500

    agregando validaciones a nombre y apellido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import { useFormik } from "formik"
 
+
+const validate = (values) => {
+  const errors = {}
+
+  if(!values.name){
+    errors.name = "requerido"
+  } else if (values.name.length < 5) {
+    errors.name = "el nombre es muy corto"
+  }
+
+  if(!values.lastname){
+    errors.lastname = "requerido"
+  } else if (values.lastname.length < 5) {
+    errors.lastname = "el apellido es muy corto"
+  }
+
+  if(!values.name){
+    errors.name = "requerido"
+  } else if (values.name.length < 5) {
+    errors.name = "el nombre es muy corto"
+  }
+  return errors
+}
+
 function App() {
 
   const formik = useFormik({
@@ -8,6 +32,7 @@ function App() {
       lastname: "",
       email: ""
     },
+    validate,
     onSubmit: values => console.log(values)
   })
 
@@ -20,6 +45,8 @@ function App() {
         onChange={formik.handleChange}
         value={formik.values.name}
       />
+      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+      <br />
       <label>Apellido</label>
       <input
         name="lastname"
@@ -27,6 +54,8 @@ function App() {
         onChange={formik.handleChange}
         value={formik.values.lastname}
       />
+      {formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
+      <br />
       <label>Email</label>
       <input
         name="email"
